405fd116cd4f16629a2d5780d9d82362eb50f63f
sleepyRita <3120461934@qq.com>
2023-11-23 12:57:06 +0800

    rgl

---
+++ src/views/spiView.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    components: {
+    },
+
+    data(){
+        return {
+            scale: '',
+        }
+    },
+}
+</script>
+
+<template>
+    计算spi
+    <div>
+        数据尺度
+        <input type="radio" value="day" name="time"/>日
+        <input type="radio" value="month" name="time"/>月
+    </div>
+
+    <div>
+        运算尺度
+        <input type="text" v-model="scale"/>
+        <input type="radio" value="day" name="scale"/>日
+        <input type="radio" value="month" name="scale"/>月
+    </div>
+
+    <div>
+        校准数据
+        <input type="date" name="startTime" value=""/>
+        <input type="date" name="endTime" value=""/>
+    </div>
+</template>
