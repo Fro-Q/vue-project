44415073753de69badb021d74f1465c3f84310cc
sleepyRita <3120461934@qq.com>
2023-11-23 12:49:22 +0800

    rlg

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,6 @@
-<script>
-// import { RouterLink, RouterView } from 'vue-router'
-import axios from 'axios'
-import { v4 as uuidv4 } from 'uuid';
-
-
-export default {
-  components: {
-  },
-
-  methods: {
-    generateRandomId() {
-      const random_id = uuidv4();
-      this.$data.random_id = random_id;
-    },
-    submitPath() {
-      const path = this.$data.path;
-      console.log('Random ID in submitPath:', this.$data.random_id);
-
-      axios.post(`http://127.0.0.1:8000/api/get_and_post/${this.$data.random_id}/`, { path })
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    },
-  },
-  data() {
-    return {
-      path: '',
-      random_id: '',
-      dataName: '',
-    };
-  },
-}
-</script>
-
 <template>
-  <header>
-    <input type="file" ref="fileInput" webkitdirectory directory @change="generateRandomId" />
-
-    <input type="text" v-model="dataName" placeholder="the name of the data you wish to see in following process"/>
-
-    <button @click="submitPath">Submit</button>
-
-  </header>
-
+  <RouterLink to="/upload">Upload</RouterLink>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import upload from '../views/uploadView.vue'
+import spi from '../views/spiView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,8 +9,19 @@ const router = createRouter({
       path: '/',
       name: 'home',
       // component: 
+    },
+    {
+      path: '/upload',
+      name: 'upload',
+      component:upload    
+    },
+    {
+      path: '/spi',
+      name: 'spi',
+      component:spi
     }
   ]
-})
+  }
+  )
 
 export default router
